186ed75c94a773ace093e0f496a58e4c05538ed5
adrianWadrzyk <adrian.wadrzyk.contact@gmail.com>
2021-02-05 14:04:00 +0100

    Add project files.

---
+++ Video/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Video
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = Console.ReadLine();
+            var timeToBeRemoved = time
+              .Replace(';', '/')
+              .Replace('-', ';')
+              .Replace('/', '-')
+              .Split(';')
+              .ToList();
+            if (timeToBeRemoved.First() == "0:00:00")
+                timeToBeRemoved.Remove("0:00:00");
+            else
+                timeToBeRemoved[0] = "0:00:00-" + timeToBeRemoved[0];
+            if (timeToBeRemoved.Last() == "2:00:00")
+                timeToBeRemoved.Remove("2:00:00");
+            else
+                timeToBeRemoved[timeToBeRemoved.Count - 1] = timeToBeRemoved.Last() + "-2:00:00";
+            var finalTime = String.Join(";", timeToBeRemoved);
+            if(finalTime[0].ToString() == "0:00:00" && finalTime[1].ToString() == "2:00:00") 
+                Console.WriteLine("empty");
+        }
+    }
+}
+
+
